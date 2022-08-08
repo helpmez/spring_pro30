01bbd0acce5c9688b39e76609bc4d5ed807e5685
helpmez <helpmez@naver.com>
2022-08-08 12:34:45 +0900

    답변형 게시판

---
+++ pro30_6/src/main/java/com/myspring/pro30/board/service/BoardServiceImpl.java
@@ -0,0 +1,105 @@
+package com.myspring.pro30.board.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.myspring.pro30.board.dao.BoardDAO;
+import com.myspring.pro30.board.vo.ArticleVO;
+import com.myspring.pro30.board.vo.ImageVO;
+
+
+@Service("boardService")
+@Transactional(propagation = Propagation.REQUIRED)
+public class BoardServiceImpl  implements BoardService{
+	@Autowired
+	private BoardDAO boardDAO;
+	
+	public Map listArticles(Map  pagingMap) throws Exception{
+		Map articlesMap = new HashMap();
+		List<ArticleVO> articlesList = boardDAO.selectAllArticlesList(pagingMap);
+		int totArticles = boardDAO.selectTotArticles();
+		articlesMap.put("articlesList", articlesList);
+		articlesMap.put("totArticles", totArticles);
+		//articlesMap.put("totArticles", 170);
+        return articlesMap;
+	}
+
+	
+	//단일 이미지 추가하기
+//	@Override
+//	public int addNewArticle(Map articleMap) throws Exception{
+//		return boardDAO.insertNewArticle(articleMap);
+//	}
+//	
+	 //다중 이미지 추가하기
+	@Override
+	public int addNewArticle(Map articleMap) throws Exception{
+		int articleNO = boardDAO.insertNewArticle(articleMap);
+		articleMap.put("articleNO", articleNO);
+		boardDAO.insertNewImage(articleMap);
+		return articleNO;
+	}
+	
+	//다중 파일 보이기
+	@Override
+	public Map viewArticle(int articleNO) throws Exception {
+		Map articleMap = new HashMap();
+		ArticleVO articleVO = boardDAO.selectArticle(articleNO);
+		List<ImageVO> imageFileList = boardDAO.selectImageFileList(articleNO);
+		articleMap.put("article", articleVO);
+		articleMap.put("imageFileList", imageFileList);
+		return articleMap;
+	}
+	
+	
+//	 //단일 파일 보이기
+//	@Override
+//	public ArticleVO viewArticle(int articleNO) throws Exception {
+//		ArticleVO articleVO = boardDAO.selectArticle(articleNO);
+//		return articleVO;
+//	}
+	
+	
+	@Override
+	public void modArticle(Map articleMap) throws Exception {
+		boardDAO.updateArticle(articleMap);
+		
+		List<ImageVO> fileList = (List<ImageVO>)articleMap.get("imageFileList");
+		List<ImageVO> modAddimageFileList = (List<ImageVO>)articleMap.get("modAddimageFileList");
+
+		if(fileList != null && fileList.size() != 0) {
+			int added_img_num = Integer.parseInt((String)articleMap.get("added_img_num"));
+			int pre_img_num = Integer.parseInt((String)articleMap.get("pre_img_num"));
+
+			if(pre_img_num < added_img_num) {  //새로 추가된 이미지가 있는 경우와 없는 경우 나누어서 처리
+				boardDAO.updateImageFile(articleMap);
+				boardDAO.insertModNewImage(articleMap);
+			}else {
+				boardDAO.updateImageFile(articleMap);
+			}
+		}else if(modAddimageFileList != null && modAddimageFileList.size() != 0) {
+			boardDAO.insertModNewImage(articleMap);
+		}
+	}
+	
+	@Override
+	public void removeArticle(int articleNO) throws Exception {
+		boardDAO.deleteArticle(articleNO);
+	}
+
+
+	@Override
+	public void removeModImage(ImageVO imageVO) throws Exception {
+		boardDAO.deleteModImage(imageVO);
+		
+	}
+	
+
+	
+}
